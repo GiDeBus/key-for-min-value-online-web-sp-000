2be1f58e1302b21bf0ed1cde67ae9acebe2ec8fd
Ignacio Del Busto <gidelbusto@gmail.com>
2020-06-15 00:42:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.each do |key,value|
    #value
-   if lowest_value < value || lowest_value == nil
+   if lowest_value == nil || lowest_value < value
       lowest_value = value
       lowest_key = key
     end
